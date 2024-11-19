09c555f2251848a06fa7075274df6ea32bc4e4e7
GustavoFantin <gustavofantinbarros@gmail.com>
2024-11-19 12:32:10 -0800

    callback hell exercise

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,17 @@
 const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
-})
+  const FirstName = data1
+  fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+  const SecondName = data2
+  fs.readFile("./age.txt", "utf-8", (err, data3) => {
+    const age = data3
+    fs.readFile("./hobbies.txt", "utf-8", (err, data4) => {
+      const hobbies = JSON.parse(data4)
+        console.log(`${FirstName} ${SecondName} is ${age} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}`)
+        })
+      })
+    })
+  })
+
+
